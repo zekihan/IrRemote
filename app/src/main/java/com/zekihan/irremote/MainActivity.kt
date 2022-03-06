3336821b6b2545299cd53deff161f58f66ded301
Zekihan AZMAN <zekihanazman99@gmail.com>
2022-03-06 15:57:17 +0300

    Initial commit

---
+++ app/src/main/java/com/zekihan/irremote/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.zekihan.irremote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.appBarMain.toolbar)
+        findViewById<>()
+    }
+}
